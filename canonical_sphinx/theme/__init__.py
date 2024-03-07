e46b8f047632ade6419ffd239cac8d8026fadc51
Tiago Nobrega <tiago.nobrega@canonical.com>
2024-03-07 14:29:46 -0300

    chore: update license to LGPL

--- canonical_sphinx/theme/__init__.py
+++ canonical_sphinx/theme/__init__.py
@@ -3,8 +3,8 @@
 # Copyright 2024 Canonical Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 3, as published
-# by the Free Software Foundation.
+# under the terms of the GNU Lesser General Public License version 3, as
+# published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
