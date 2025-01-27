e779437fc2959979dc1040b3b862f5f98a6bad01
MengT <meng.tang@canonical.com>
2025-01-27 20:04:56 +0800

    feat: update variable in example conf

--- example/conf.py
+++ example/conf.py
@@ -144,10 +144,11 @@ github_username = "example"
 github_repository = "project"
 
 # PDF LaTeX configuration: a list of tuples
+# Removing the variable will revert to the default configuration for latexpdf.
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-latex_documents
 latex_documents = [(
-    master_doc, # startdocname, master_doc is the default project root index
-    "",         # targetname: file name of the output PDF. Whitespace is not allowed.
+    "index", # startdocname, the project root index
+    f"{project.replace(' ', '_')}.tex", # targetname: file name of the output PDF. Whitespace is not allowed.
     project,    # title: use empty string if you want to use the title of the master_doc
     author,     # author
     "manual",   # theme
