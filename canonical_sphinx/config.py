6af9cab16f1903e295fe57cad549fc5781ee016c
Michael DuBelko <michael.dubelko@canonical.com>
2026-09-09 09:36:50 -0700

    fix: use 'info' log level for deprecated config (#125)

--- canonical_sphinx/config.py
+++ canonical_sphinx/config.py
@@ -262,16 +262,19 @@ def config_inited(app: Sphinx, config: SphinxConfig) -> None:  # noqa: PLR0915, 
 
     # Warnings for old HTML context settings
     if "github_folder" in config.html_context:
-        logger.warning(
-            "conf.py setting 'github_folder' is deprecated. Use 'repo_folder' instead.",
+        logger.info(
+            f"{__name__.partition('.')[0]}: conf.py: 'github_folder' is deprecated. "
+            "Use 'repo_folder' instead.",
+            color="yellow",
         )
         folder = config.html_context["github_folder"]
         config.html_context["repo_folder"] = folder
 
     if "github_version" in config.html_context:
-        logger.warning(
-            "conf.py setting 'github_version' is deprecated. Use 'repo_default_branch' "
-            "instead.",
+        logger.info(
+            f"{__name__.partition('.')[0]}: conf.py: 'github_version' is deprecated. "
+            "Use 'repo_default_branch' instead.",
+            color="yellow",
         )
         default_branch = config.html_context["github_version"]
         config.html_context["repo_default_branch"] = default_branch
